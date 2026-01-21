d0471e920a6e68ec245a64c94186669a00969b21
haris <harisakbar5566@gmail.com>
2026-01-21 17:59:44 +0500

    create security teacher read-only access

--- __manifest__.py
+++ __manifest__.py
@@ -9,10 +9,13 @@
         'account'
     ], # Monetary fields remain safe
     'data': [
+    'security/security.xml',
     'security/ir.model.access.csv',
+    'security/record_rules.xml',
 
         'reports/student_action.xml',
         'reports/student_template.xml',
+
     'views/menu.xml',
     'views/student.xml',
     'views/classroom.xml',
--- models/teacher.py
+++ models/teacher.py
@@ -1,15 +1,37 @@
 from odoo import models, fields
 
+
 class Teacher(models.Model):
     _name = "school.teacher"
     _description = "Teacher"
 
+    # BASIC INFO
     name = fields.Char(string="Name", required=True)
     phone = fields.Char(string="Phone")
     email = fields.Char(string="Email")
-    subject_ids = fields.Many2many('school.subject', string="Subjects")
+    section = fields.Char(string="Section")
+
+    teacher_id = fields.Many2one(
+        'school.teacher',
+        string="Teacher"
+    )
+    # LINK TO USER (VERY IMPORTANT FOR SECURITY)
+    user_id = fields.Many2one(
+        'res.users',
+        string="Related User",
+        help="User account linked with this teacher"
+    )
+
+    # ACADEMIC INFO
+    subject_ids = fields.Many2many(
+        'school.subject',
+        string="Subjects"
+    )
+
+    # SALARY INFO
     salary = fields.Monetary(string="Salary")
     currency_id = fields.Many2one(
         'res.currency',
+        string="Currency",
         default=lambda self: self.env.company.currency_id
     )
